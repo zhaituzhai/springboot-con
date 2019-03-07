1a4e491b654983de755d0edd04f207ff27df8e5a
zhaojiamin <zhaojiamin@szhtxx.com>
2019-03-07 18:58:33 +0800

    add swagger fail commit

--- springboot-hello/src/main/java/com/zhaojm/config/web/WebConfig.java
+++ springboot-hello/src/main/java/com/zhaojm/config/web/WebConfig.java
@@ -1,6 +1,7 @@
 package com.zhaojm.config.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
@@ -19,8 +20,9 @@ public class WebConfig extends WebMvcConfigurationSupport {
      * https://www.cnblogs.com/deng720/p/8989388.html
      * spring boot 继承WebMvcConfigurationSupport后自动配置不生效问题
      */
-    /*@Override
+    @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
-    }*/
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+    }
 }
--- springboot-hello/src/main/java/com/zhaojm/controller/HelloController.java
+++ springboot-hello/src/main/java/com/zhaojm/controller/HelloController.java
@@ -1,21 +1,34 @@
 package com.zhaojm.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Api(tags = "hello",value = "first")
 @RestController
 public class HelloController {
     
     @GetMapping
-    public Map<String, String> hello(Map<String, String> map){
+    @ApiOperation("hello")
+    public Map<String, String> hello(@ApiParam("map") Map<String, String> map){
         
         map = new HashMap<String, String>();
         map.put("name", "zhaojiamin");
         map.put("age", "34");
         
+        return map;
+    }
+
+    @GetMapping
+    public Map<String, String> getUser(){
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("zhaojm", "m");
+        map.put("joy", "m");
         return map;
     }
 
