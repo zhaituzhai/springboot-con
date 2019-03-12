db3f972c03dcc4baee6dc892024c2c6924e6e3cd
zhaojiamin <zhaojiamin@szhtxx.com>
2019-03-12 18:22:32 +0800

    add security project

---
+++ spring-secutity/src/main/java/com/zhaojm/secutity/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.zhaojm.secutity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HelloController {
+
+    @GetMapping
+    public String hello(){
+        return "Hello";
+    }
+
+    @PostMapping("/login")
+    public String login(){
+        return "login.html";
+    }
+}
