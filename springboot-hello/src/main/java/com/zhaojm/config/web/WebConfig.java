6455bf313024696096dc8975c58b778ae4c1b4c6
zhaojiamin <zhaojiamin@szhtxx.com>
2019-03-20 11:21:22 +0800

    add static can't read

--- springboot-hello/src/main/java/com/zhaojm/config/web/WebConfig.java
+++ springboot-hello/src/main/java/com/zhaojm/config/web/WebConfig.java
@@ -23,7 +23,7 @@ public class WebConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         //将所有/static/** 访问都映射到classpath:/static/ 目录下
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
         //swagger2
         registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
---
+++ springboot-hello/src/main/java/com/zhaojm/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.zhaojm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+}
